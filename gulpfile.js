e08ac05ef8b7c22083f91972f0fccd475967fa09
Pedro Badaro <badaro1988@gmail.com>
2025-04-14 12:29:34 +0800

    Corrige configurações de build para o Vercel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require ('sass'))
+const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify');
 
 function scripts() {
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
 }
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed' }))
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
@@ -31,10 +31,31 @@ function fonts() {
         .pipe(gulp.dest('./dist/fonts'));
 }
 
-exports.default = gulp.parallel(styles, images, scripts, html, fonts);
-
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-    gulp.watch('./*.html', gulp.parallel(html));
+// Função para copiar arquivos estáticos adicionais
+function static() {
+    return gulp.src([
+        './src/images/**/*',
+        './src/fonts/**/*',
+        './src/icons/**/*'
+    ], { base: './src' })
+    .pipe(gulp.dest('./dist'));
 }
+
+// Task de build
+exports.build = gulp.series(
+    gulp.parallel(styles, images, scripts, fonts),
+    html,
+    static
+);
+
+// Task padrão
+exports.default = exports.build;
+
+// Task de watch para desenvolvimento
+exports.watch = function() {
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/**/*.js', gulp.parallel(scripts));
+    gulp.watch('./*.html', gulp.parallel(html));
+    gulp.watch('./src/images/**/*', gulp.parallel(images));
+    gulp.watch('./assets-fonts/**/*', gulp.parallel(fonts));
+};
