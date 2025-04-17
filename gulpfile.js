59b5d2b7ca7675a08af175fa74cbd773911fb398
Pedro Badaro <badaro1988@gmail.com>
2025-04-17 12:15:24 +0800

    fix: restaura configuração original do gulpfile e ajusta estrutura de arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,21 @@
-import gulp from 'gulp';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import imagemin from 'gulp-imagemin';
-import uglify from 'gulp-uglify';
-
-const sass = gulpSass(dartSass);
-
-function scripts() {
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/dist/js'))
-}
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./build/dist/css'));
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+    return gulp.src(['./src/images/**/*'], { encoding: false })
         .pipe(imagemin())
-        .pipe(gulp.dest('./build/dist/images'));
+        .pipe(gulp.dest('./dist/images'));
 }
 
-function html() {
-    return gulp.src('./src/index.html')
-        .pipe(gulp.dest('./build'));
+exports.default = gulp.parallel(styles, images,);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
-
-function clean(cb) {
-    return gulp.src('./build', {allowEmpty: true})
-        .pipe(gulp.dest('./build'));
-}
-
-export const build = gulp.series(clean, gulp.parallel(styles, images, scripts, html));
-
-export const watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-    gulp.watch('./src/index.html', gulp.parallel(html));
-}
-
-export default build;
